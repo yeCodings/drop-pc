b174f3d0c4da16d3f85045b58c125b8bc7d6c8c9
yd <yedong@yddeMacBook-Air.padavan>
2023-08-25 21:09:28 +0800

    feat:eslint配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,29 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    require.resolve('eslint-config-airbnb'),
+    require.resolve('eslint-config-airbnb/hooks'),
+    require.resolve('eslint-config-airbnb-typescript'),
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  parser: require.resolve('@typescript-eslint/parser'),
+  parserOptions: {
+    project: require.resolve('./tsconfig.json'),
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/function-component-definition': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/exhaustive-deps': 0,
   },
+  settings: {
+    react: {
+      'version': 'detect'
+    }
+  }
 }
